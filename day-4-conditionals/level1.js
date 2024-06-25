8dcba1b4a66ad759728e1cc7b69d715da9281052
Abdullahi(busybrain) <awwalhajine@gmail.com>
2024-06-25 11:23:17 +0100

    level 2 done

---
+++ day-4-conditionals/level1.js
@@ -0,0 +1,59 @@
+// Level 1
+/*
+1. Get user input using prompt(“Enter your age:”). If user is 18 or older , give feedback:'You are old enough to drive' but if not 18 give another feedback stating to wait for the number of years he needs to turn 18. 
+*/
+
+// const userAge = prompt("Enter your age:");
+// if (userAge >= 18) {
+//   console.log("You are old enough to drive");
+// } else if (userAge < 18) {
+//   let ageToDrive = 18 - parseFloat(userAge);
+//   console.log(`You are left with ${ageToDrive} years old to drive`);
+// }
+
+/*
+2. Compare the values of myAge and yourAge using if … else. Based on the comparison and log the result to console stating who is older (me or you). Use prompt(“Enter your age:”) to get the age as input.
+*/
+
+// const myAge = 28;
+// let yourAge = prompt("Enter your age: ");
+// yourAge = parseFloat(yourAge);
+
+// if (yourAge > myAge) {
+//   let age = yourAge - myAge;
+//   console.log(`You are ${age} years older than me.`);
+// } else if (myAge > yourAge) {
+//   age = myAge - yourAge;
+//   console.log(`I am ${age} years older than you`);
+// } else {
+//   console.log("We are age mates");
+// }
+
+/**
+ * 3. If a is greater than b return 'a is greater than b' else 'a is less than b'. Try to implement it in two ways
+ */
+
+// let a = 30;
+// let b = 20;
+
+// if (a > b) {
+//   console.log(`${a} is greater than ${b}`);
+// } else {
+//   console.log(`${b} is greater than ${a}`);
+// }
+
+// a = 40;
+// b = 50;
+
+// let num = a > b ? `${a} is greater than ${b}` : `${b} is greater than ${a}`;
+
+// console.log(num);
+
+// let num = prompt("Enter a number");
+// num = parseFloat(num);
+
+// if (num % 2 === 0) {
+//   console.log(`${num} is an even number`);
+// } else {
+//   console.log(`${num} is an odd number`);
+// }
---
+++ day-4-conditionals/level2.js
@@ -0,0 +1,63 @@
+/**
+ * Exercises: Level 2
+1. Write a code which can give grades to students according to theirs scores:
+
+    80-100, A
+    70-89, B
+    60-69, C
+    50-59, D
+    0-49, F
+ */
+
+// let score = prompt("Enter the score:");
+// score = parseFloat(score);
+
+// if (score <= 100 && score >= 90) {
+//   console.log("A");
+// } else if (score <= 89 && score >= 70) {
+//   console.log("B");
+// } else if (score <= 69 && score >= 60) {
+//   console.log("C");
+// } else if (score <= 59 && score >= 50) {
+//   console.log("D");
+// } else {
+//   console.log("F");
+// }
+
+// let season = prompt("Type the season");
+
+// if (season === "October" || season === "November") {
+//   console.log("The season is Autumn");
+// } else if (
+//   season === "December" ||
+//   season === "January" ||
+//   season === "February"
+// ) {
+//   console.log("The season is Winter");
+// } else if (season === "March" || season === "April" || season === "May") {
+//   console.log("Spring");
+// } else if (season === "June" || season === "July" || season === "August") {
+//   console.log("Summer");
+// } else {
+//   console.log(`${season} is not a valid entry`);
+// }
+
+/**
+ * 3 Check if a day is weekend day or a working day. Your script will take day as an input.
+ */
+
+let day = prompt(`Enter a day (Monday to Sunday)`);
+
+if (
+  day === "Monday" ||
+  day === "Tuesday" ||
+  day === "Wednesday" ||
+  day === "Thursday" ||
+  day === "Friday"
+) {
+  console.log(`${day} is a working day`);
+} else if (day === "Saturday" || day === "Sunday") {
+  console.log(`${day} is a weekend`);
+} else {
+  console.log(`${day} is not a valid day of the week`);
+}
